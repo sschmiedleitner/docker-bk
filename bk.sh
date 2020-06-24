e47507b905b8d7bdac9b138a7c35bc2414e138bd
sschmiedleitner <46022765+sschmiedleitner@users.noreply.github.com>
2020-06-24 10:31:14 +0200

    Update bk.sh

--- bk.sh
+++ bk.sh
@@ -6,7 +6,7 @@ while [ $secsSinceMidnight -lt 50000 ]
 do
   sleep $(( ( RANDOM % 10 )  + 1 ))s
   secsSinceMidnight=$(( $(date +%s) - $(date -d '00:00:00' +%s) ))
-# ./linux_386_chrome-remote-reload
+  /home/seluser/linux_386_chrome-remote-reload
   echo $secsSinceMidnight
 done
 
